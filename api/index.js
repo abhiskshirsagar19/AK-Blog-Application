7c64dc25aa04d428514e2528c502096395947a1e
Abhi <abhikshirsagar1999@gmail.com>
2024-12-19 19:12:37 +0530

    The Databse connection is Established.

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log("Connection Established");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 const app = express();
 
 app.listen(3000, () => {
