0d44f038dd45aea422da6d2ccc183e36fedd5c38
Abhi <abhikshirsagar1999@gmail.com>
2025-01-05 21:33:30 +0530

    The like functionality added to the comment section.

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -7,7 +7,7 @@ export const test = (req, res) => {
 };
 
 export const updateUser = async (req, res, next) => {
-  console.log(req.user.id + " " + req.params.userId);
+  // console.log(req.user.id + " " + req.params.userId);
   if (req.user.id !== req.params.userId) {
     return next(
       errorHandler(403, "You are not authorized to update this user")
