4b9fa672dbd3b3ceb014bd5fad76ed8b6dda3f0f
Abhi <abhikshirsagar1999@gmail.com>
2025-01-06 22:46:28 +0530

    The done the minor changes now ready to go live.

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div>
+      <div>
+        <div>
+          <h1>About AK's Blog</h1>
+          <div>
+            <p></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,5 +1,10 @@
-import React from "react";
-
+import CallToAction from "../components/CallToAction";
 export default function Projects() {
-  return <div>Projects</div>;
+  return (
+    <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6">
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <p></p>
+      <CallToAction />
+    </div>
+  );
 }
