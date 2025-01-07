ca8c416830443a2d7cd6233691ae4babbe9b594d
Abhi <abhikshirsagar1999@gmail.com>
2025-01-07 10:20:50 +0530

    The target added to vite.config.

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,16 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    proxy: { "/api": { target: "http://localhost:3000", secure: false } },
+    proxy: {
+      "/api": {
+        target: "https://abhik-blog-application.vercel.app",
+        secure: false,
+      },
+    },
   },
   plugins: [react()],
   define: {
     "process.env": {},
   },
 });
+ad;
