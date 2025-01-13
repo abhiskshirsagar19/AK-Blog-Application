0ddb97cb95b93f3ebb0543419e21a1253a2c9ca5
Abhi <abhikshirsagar1999@gmail.com>
2025-01-13 13:05:06 +0530

    The Comments section is only visible to Admin not user. False logic changed.

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -91,15 +91,17 @@ export default function DashSidebar() {
               </Sidebar.Item>
             </Link>
           )}
-          <Link to="/dashboard?tab=comments">
-            <Sidebar.Item
-              active={tab === "comments"}
-              icon={HiAnnotation}
-              as="div"
-            >
-              Comments
-            </Sidebar.Item>
-          </Link>
+          {currentUser.isAdmin && (
+            <Link to="/dashboard?tab=comments">
+              <Sidebar.Item
+                active={tab === "comments"}
+                icon={HiAnnotation}
+                as="div"
+              >
+                Comments
+              </Sidebar.Item>
+            </Link>
+          )}
           <Sidebar.Item
             icon={HiArrowSmRight}
             className="cursor-pointer"
